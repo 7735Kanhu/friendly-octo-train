4dae85fd72744cbc7dc1e5226faef2ab5c15b7f8
Kanhu Charan <kanhu2551996@gmail.com>
2024-05-29 07:42:24 +0530

    wallet add

--- app/(drawer)/(tabs)/wallet/index.jsx
+++ app/(drawer)/(tabs)/wallet/index.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 export default function Page() {
   return (
@@ -9,7 +10,7 @@ export default function Page() {
         <Text style={styles.rupees}> ₹ 1902.00</Text>
         <Text style={styles.balanceText}>Avaliable Balance</Text>
       </View>
-      <View style={styles.bottomwallet} className='bg-gray-100'>
+      <View style={styles.bottomwallet} className='bg-gray-100 space-y-3'>
           <View style={styles.totalIncom} className='bg-white shadow-xl py-6 px-5'>
             <View className='flex flex-row gap-2'>
             <AntDesign name="plussquare" size={40} color="green" />
@@ -24,6 +25,28 @@ export default function Page() {
               <Text className='font-bold text-md'>₹ 2460 </Text>
               <Text className='text-xs'>Total Expenditure </Text>
             </View>
+            </View>
+          </View>
+          <View className='w-full flex flex-row bg-white shadow-xl px-5 py-6 rounded-xl justify-between'>
+            <View style={styles.service} className='flex align-middle justify-center text-center gap-3'>
+            <FontAwesome6 name="money-bill-1" size={35} color="green" className=''/>
+            <Text className='font-semibold'>Add Money</Text>
+            </View>
+            <View style={styles.service} className='flex align-middle justify-center text-center gap-3'>
+            <FontAwesome6 name="money-check" size={35} color="orange" className=''/>
+            <Text className='font-semibold'>Withdraw</Text>
+            </View>
+            <View style={styles.service} className='flex align-middle justify-center text-center gap-3'>
+            <FontAwesome6 name="money-bill-transfer" size={35} color="blue" className=''/>
+            <Text className='font-semibold'>Transfor</Text>
+            </View>
+          </View>
+          <Text className='font-bold text-lg m-2'>Wallet History</Text>
+          <View>
+            <View>
+              <Text>All</Text>
+              <Text>Deposite</Text>
+              <Text>Withdraw</Text>
             </View>
           </View>
       </View>
@@ -62,5 +85,11 @@ const styles = StyleSheet.create({
     justifyContent:'space-between',
     padding:5,
     borderRadius:15
+  },
+  service:{
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    // marginHorizontal:'auto'
   }
 })
