43b348f162fd658177ae6ee47a9638bfbc4b00b5
Kanhu Charan <kanhu2551996@gmail.com>
2024-05-30 07:41:17 +0530

    wallet edit

--- app/(drawer)/(tabs)/wallet/index.jsx
+++ app/(drawer)/(tabs)/wallet/index.jsx
@@ -1,7 +1,9 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome6 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 export default function Page() {
   return (
@@ -41,13 +43,47 @@ export default function Page() {
             <Text className='font-semibold'>Transfor</Text>
             </View>
           </View>
-          <Text className='font-bold text-lg m-2'>Wallet History</Text>
+          <Text className='font-bold text-lg m-2 mb-1'>Wallet History</Text>
           <View>
-            <View>
-              <Text>All</Text>
-              <Text>Deposite</Text>
-              <Text>Withdraw</Text>
+            <View className='flex flex-row gap-6 px-3 mb-3'>
+              <Text className='font-semibold text-md bg-green-600 border border-green-600 rounded-full p-1 px-2 text-white'>All</Text>
+              <Text className='font-semibold text-md p-1 px-2'>Deposite</Text>
+              <Text className='font-semibold text-md p-1 px-2'>Withdraw</Text>
             </View>
+            {/* <ScrollView style={{flex:1}}> */}
+            <View className='flex flex-col gap-1'>
+              <View className='bg-white p-2 rounded-lg shadow-lg flex flex-row justify-between align-middle items-center'>
+              <View className='flex flex-row gap-5 items-center'>
+              <Entypo name="arrow-down" size={26} color="green" />
+              <View>
+                <Text className='text-gray-400 text-xs'>Today 2.30</Text>
+                <Text className='text-lg font-bold'>Cash Deposite</Text>
+              </View>
+              </View>
+              <Text className='text-lg font-bold text-green-700'>+ 870.00</Text>
+              </View>
+              <View className='bg-white p-2 rounded-lg shadow-lg flex flex-row justify-between align-middle items-center'>
+              <View className='flex flex-row gap-5 items-center'>
+              <Entypo name="arrow-up" size={26} color="red" />
+              <View>
+                <Text className='text-gray-400 text-xs'>Today 2.30</Text>
+                <Text className='text-lg font-bold'>Cash Withdraw</Text>
+              </View>
+              </View>
+              <Text className='text-lg font-bold text-red-500'>- 500.00</Text>
+              </View>
+              <View className='bg-white p-2 rounded-lg shadow-lg flex flex-row justify-between align-middle items-center'>
+              <View className='flex flex-row gap-5 items-center'>
+              <Entypo name="arrow-down" size={26} color="green" />
+              <View>
+                <Text className='text-gray-400 text-xs'>Today 2.30</Text>
+                <Text className='text-lg font-bold'>Cash Deposite</Text>
+              </View>
+              </View>
+              <Text className='text-lg font-bold text-green-700'>+ 870.00</Text>
+              </View>
+            </View>
+            {/* </ScrollView> */}
           </View>
       </View>
     </View>
